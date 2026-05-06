ee71939b9f12e698284ab4cd429ef0c0a050970a
kkairofreitas-glitch <kkairo.freitas@gmail.com>
2026-05-05 21:00:14 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+{
+  "name": "RealStock Coleta",
+  "short_name": "RealStock",
+  "start_url": "/coleta-mobile",
+  "display": "standalone",
+  "background_color": "#0a1f3d",
+  "theme_color": "#0a1f3d",
+  "icons": [
+    {
+      "src": "/icones/icone-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/icones/icone-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
